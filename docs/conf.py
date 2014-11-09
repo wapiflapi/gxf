bc32fd6112a560c0a4991f958f93a39cadf4511f
Wannes Rombouts <wapiflapi@yahoo.fr>
2014-11-09 01:59:21 +0100

    Use readthedocs theme localy if available.

--- docs/conf.py
+++ docs/conf.py
@@ -113,6 +113,19 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+# if we are not on readthedocs.org we still load their theme cause its cool.
+if not os.environ.get('READTHEDOCS', None) == 'True':
+
+    try:
+        import sphinx_rtd_theme
+    except ImportError:
+        pass
+
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify its
+
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
